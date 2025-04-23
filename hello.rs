94eb2edc0b30300f147bc9bd3b9b23bdb67f7d2d
Balogun <razak2free@gmail.com>
2025-04-23 20:49:01 +0100

    Initial commit

---
+++ hello.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let x: i32 = -42;
+    let y: u64 = 128;
+ 
+   println!("unSigned Int: {}", x);
+   println!("Signed Int: {}", y);
+ 
+ let e: i32 = 214748349;
+ let i: i64 = 922337206854775808;
+ 
+    println!("max for i32: {}", e);
+    println!("max for i64: {}", i);
+   
+ }
+ 

7baa771ef8fb8f359442dba635e2409c0e5ffa5e
Balogun <razak2free@gmail.com>
2025-04-24 09:08:39 +0100

    update: chnages

--- hello.rs
+++ hello.rs
@@ -1,15 +1,20 @@
 fn main() {
     let x: i32 = -42;
     let y: u64 = 128;
- 
-   println!("unSigned Int: {}", x);
-   println!("Signed Int: {}", y);
- 
- let e: i32 = 214748349;
- let i: i64 = 922337206854775808;
- 
+
+    println!("unSigned Int: {}", x);
+    println!("Signed Int: {}", y);
+
+    let e: i32 = 214748349;
+    let i: i64 = 922337206854775808;
+
     println!("max for i32: {}", e);
     println!("max for i64: {}", i);
-   
- }
- 
+
+    let pi: f32 = 3.14;
+    println!("Value of pi: {}", pi);
+
+    // Boolean
+    let is_snowing: bool = false;
+    println!("Is it snowing? {}", is_snowing);
+}
